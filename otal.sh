a06f5cc05f324fce89f7ebe30b14ae9cc7af2821
DavidReinon <darega@floridauniversitaria.es>
2023-02-14 15:33:09 +0100

    otal.sh

---
+++ otal.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+repetir=true
+while [[ $repetir == true ]]; do
+
+    read -p "Introduce la ciudad: " ciudad
+    ciudadLimite=$(cat consumos.txt | grep $ciudad -i -c)
+
+    if [ $ciudadLimite -eq 0 ]; then
+        echo "Ciudad no encontrada. Vuelve a introducir"
+        repetir=true
+    else
+        for i in $(seq 1 $ciudadLimite); do
+            consumo=$(cat consumos.txt | grep $ciudad -i | head -n$i | tail -n1 | awk '{print $4}')
+            total=$(($total + $consumo))
+        done
+        echo "El total de consumo de $ciudad es de $total"
+        repetir=false
+    fi
+done
